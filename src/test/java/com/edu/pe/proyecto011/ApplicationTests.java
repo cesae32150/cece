c1837e597aa3bf3dc5a0649cc9f43d2ffe6dfb2f
LAB_C_REAL <labc@continental.edu.pe>
2023-10-28 05:33:14 -0500

    version

---
+++ src/test/java/com/edu/pe/proyecto011/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.pe.proyecto011;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
